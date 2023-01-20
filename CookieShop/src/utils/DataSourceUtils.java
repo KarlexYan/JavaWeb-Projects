c7aa612d763ddf8e24673dc9151626a0e8c1d24d
KarlexYan <KarlexYan@aliyun.com>
2023-01-20 23:54:30 +0800

    工具类DataSourceUtils 用来获取DataSource和Connection

--- CookieShop/src/utils/DataSourceUtils.java
+++ CookieShop/src/utils/DataSourceUtils.java
@@ -5,7 +5,7 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
-
+// 工具类DataSourceUtils 用来获取DataSource和Connection
 public class DataSourceUtils {
     private static DataSource ds=new ComboPooledDataSource();
 
