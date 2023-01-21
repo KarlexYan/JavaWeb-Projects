1e2ebdf974e68cc92797c50339e031e1655510d1
KarlexYan <KarlexYan@aliyun.com>
2023-01-21 16:54:55 +0800

    首页控制层

--- CookieShop/src/servlet/IndexServlet.java
+++ CookieShop/src/servlet/IndexServlet.java
@@ -11,13 +11,16 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+// 首页控制层
 @WebServlet(name = "IndexServlet",urlPatterns = "/index")
 public class IndexServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request, response);
     }
+    // 私有化变量
     private GoodsService gService=new GoodsService();
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 调用
         List<Map<String,Object>> ScrollGood=gService.getScrollGood();
         request.setAttribute("scroll",ScrollGood);
 
