325560213c2c4c97ca8376ba0a2836158e9b365f
KarlexYan <KarlexYan@aliyun.com>
2023-01-21 16:15:30 +0800

    全局编码拦截器

--- CookieShop/src/filter/EncodeFilter.java
+++ CookieShop/src/filter/EncodeFilter.java
@@ -4,13 +4,16 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
+// 全局编码拦截器
 @WebFilter(filterName = "EncodeFilter",urlPatterns = "/*")
 public class EncodeFilter implements Filter {
     public void destroy() {
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        // 设置全局编码为utf-8
         req.setCharacterEncoding("utf-8");
+        // 放行
         chain.doFilter(req, resp);
     }
 
