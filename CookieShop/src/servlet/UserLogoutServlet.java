9cef71f51af2b1704ea5e292f4754b6bc29f4c5e
KarlexYan <KarlexYan@aliyun.com>
2023-01-21 15:24:11 +0800

    用户退出登录控制层

--- CookieShop/src/servlet/UserLogoutServlet.java
+++ CookieShop/src/servlet/UserLogoutServlet.java
@@ -7,14 +7,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+// 用户退出登录控制层
 @WebServlet(name = "user_logout",urlPatterns = "/user_logout")
 public class UserLogoutServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 从session中移除user变量
         request.getSession().removeAttribute("user");
+        // 页面跳转回首页
         response.sendRedirect("/index");
     }
 }
