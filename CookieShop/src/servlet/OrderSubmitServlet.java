c9b13f56f03632bced770bc752be899a976f3f39
KarlexYan <KarlexYan@aliyun.com>
2023-01-29 17:55:55 +0800

    订单提交控制层

--- CookieShop/src/servlet/OrderSubmitServlet.java
+++ CookieShop/src/servlet/OrderSubmitServlet.java
@@ -7,17 +7,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+// 订单提交控制层
 @WebServlet(name = "order_submit",urlPatterns = "/order_submit")
 public class OrderSubmitServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 从session获取user变量
         if(request.getSession().getAttribute("user")!=null) {
+            // 如果不为空，说明已登录，将请求转发到订单提交页
             request.getRequestDispatcher("/order_submit.jsp").forward(request, response);
         }else {
+            // 如果为空，提示需要登录后再提交
             request.setAttribute("failMsg", "请登录后，再提交订单！");
+            // 将请求转发到用户登录页
             request.getRequestDispatcher("/user_login.jsp").forward(request, response);
         }
     }
