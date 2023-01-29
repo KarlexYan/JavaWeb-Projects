209c9d15d16e6f9ca31be978789b7630320c61f8
KarlexYan <KarlexYan@aliyun.com>
2023-01-29 17:28:19 +0800

    购物车删除控制层

--- CookieShop/src/servlet/GoodsDeleteServlet.java
+++ CookieShop/src/servlet/GoodsDeleteServlet.java
@@ -9,16 +9,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+// 购物车删除控制层
 @WebServlet(name = "goods_delete",urlPatterns = "/goods_delete")
 public class GoodsDeleteServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 从session查找order变量，将获取到的对象转化为Order对象
         Order o = (Order) request.getSession().getAttribute("order");
+        // 将请求中的goodsid转换为int类型，传入变量goodsid中
         int goodsid = Integer.parseInt(request.getParameter("goodsid"));
+        // 从itemMap中移除商品
         o.delete(goodsid);
+        // 响应ok
         response.getWriter().print("ok");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doPost(request, response);
     }
 }
