cb5a683839da5211db4906c3e2482a7061949cd6
KarlexYan <KarlexYan@aliyun.com>
2023-01-31 19:51:36 +0800

    后台删除商品控制层

--- CookieShop/src/servlet/AdminGoodsDeleteServlet.java
+++ CookieShop/src/servlet/AdminGoodsDeleteServlet.java
@@ -8,16 +8,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+// 后台删除商品控制层
 @WebServlet(name = "admin_goods_delete",urlPatterns = "/admin/goods_delete")
 public class AdminGoodsDeleteServlet extends HttpServlet {
+    // 私有化商品业务层
     private GoodsService gService = new GoodsService();
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 从请求中获取变量id的值，即商品的id
         int id = Integer.parseInt(request.getParameter("id"));
+        // 调用商品业务层，根据商品id删除商品
         gService.delete(id);
+        // 执行完毕后将请求转发到后台商品管理列表页
         request.getRequestDispatcher("/admin/goods_list").forward(request, response);
     }
 
