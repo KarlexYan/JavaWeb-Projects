9745246036530a5b86be0cd5d3a865a7ac75fb80
KarlexYan <KarlexYan@aliyun.com>
2023-01-31 19:51:01 +0800

    后台点击商品修改跳转页面的数据显示控制层

--- CookieShop/src/servlet/AdminGoodsEditshowServelt.java
+++ CookieShop/src/servlet/AdminGoodsEditshowServelt.java
@@ -9,17 +9,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+// 后台点击商品修改跳转页面的数据显示控制层
 @WebServlet(name = "admin_goods_editshow",urlPatterns = "/admin/goods_editshow")
 public class AdminGoodsEditshowServelt extends HttpServlet {
+    // 私有化商品业务层
     private GoodsService gService = new GoodsService();
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 从请求中获取变量id的值，即商品id
         int id = Integer.parseInt(request.getParameter("id"));
+        // 调用商品业务层，根据id获取对应的商品对象
         Goods g = gService.getGoodsById(id);
+        // 将商品对象写入到请求中
         request.setAttribute("g", g);
+        // 将请求转发到后台商品编辑页
         request.getRequestDispatcher("/admin/goods_edit.jsp").forward(request, response);
     }
 
