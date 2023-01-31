c10fecb16e19d5f6d430c562c5d98fda0b888997
KarlexYan <KarlexYan@aliyun.com>
2023-01-31 19:51:21 +0800

    后台商品修改页面的表单数据操作控制层

--- CookieShop/src/servlet/AdminGoodsEditServlet.java
+++ CookieShop/src/servlet/AdminGoodsEditServlet.java
@@ -18,22 +18,32 @@ import java.io.InputStream;
 import java.util.Date;
 import java.util.List;
 
+// 后台商品修改页面的表单数据操作控制层
 @WebServlet(name = "admin_goods_edit",urlPatterns = "/admin/goods_edit")
 public class AdminGoodsEditServlet extends HttpServlet {
+    // 私有化商品业务层对象
     private GoodsService gService = new GoodsService();
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 创建FileItem对象的工厂，用于实现文件上传
         DiskFileItemFactory factory=new DiskFileItemFactory();
+        // 创建Servlet上传工具对象，用于实现Servlet文件上传
         ServletFileUpload upload = new ServletFileUpload(factory);
         try {
+            // 解析出请求中form表单中的每个字段的数据，并将它们分别包装成独立的Fileltem对象
             List<FileItem> list = upload.parseRequest(request);
+            // 创建商品对象
             Goods g = new Goods();
+            // 初始化页码和typeid
             int pageNumber =1;
             int type=0;
+            // 遍历表单对象list，判断是否为普通字段
             for(FileItem item:list) {
+                // 如果是普通字段
                 if(item.isFormField()) {
+                    // 获取普通字段及其对应的值，使用switch筛选关键字，设置Good商品对象的值
                     switch(item.getFieldName()) {
                         case "id":
                             g.setId(Integer.parseInt(item.getString("utf-8")));
@@ -70,11 +80,21 @@ public class AdminGoodsEditServlet extends HttpServlet {
                             break;
                     }
                 }else {
+                    // 如果不是普通字段，说明是文件，读取出来，保存到服务器端
+                    // FileItem.getInputStream() 以流的形式返回上传文件的数据内容
+                    // InputStream.available() 获取数据流里有多少个字节可以被读取
+                    // 如果可以被读取字节 <=0 ,跳过本次循环
                     if(item.getInputStream().available()<=0)continue;
+
+                    // 获取文件名
                     String fileName = item.getName();
+                    // 截取文件后缀名
                     fileName = fileName.substring(fileName.lastIndexOf("."));
+                    // 修改文件名格式，以时间毫秒数命名，添加后缀名
                     fileName = "/"+new Date().getTime()+fileName;
+                    // 创建路径，连接上fileName，组成一条完整的文件路径
                     String path = this.getServletContext().getRealPath("/picture")+fileName;
+                    // 读取并保存到服务端
                     InputStream in = item.getInputStream();
                     FileOutputStream out = new FileOutputStream(path);
                     byte[] buffer = new byte[1024];
@@ -82,9 +102,13 @@ public class AdminGoodsEditServlet extends HttpServlet {
                     while( (len=in.read(buffer))>0 ) {
                         out.write(buffer);
                     }
+
+                    // 释放资源
                     in.close();
                     out.close();
                     item.delete();
+
+                    // 使用switch筛选关键字，设置商品对象里的图片路径
                     switch(item.getFieldName()) {
                         case "cover":
                             g.setCover("/picture"+fileName);
@@ -98,7 +122,9 @@ public class AdminGoodsEditServlet extends HttpServlet {
                     }
                 }
             }
+            // 调用商品业务层，更新商品信息
             gService.update(g);
+            // 执行后，将请求转发到原来的商品管理列表页
             request.getRequestDispatcher("/admin/goods_list?pageNumber="+pageNumber+"&type="+type).forward(request, response);
         } catch (FileUploadException e) {
             // TODO Auto-generated catch block
