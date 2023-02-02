a930974e67362aa8037bf388418359254780552e
KarlexYan <KarlexYan@aliyun.com>
2023-02-02 16:43:39 +0800

    后台类目列表修改操作控制层

--- CookieShop/src/servlet/AdminTypeEditServlet.java
+++ CookieShop/src/servlet/AdminTypeEditServlet.java
@@ -11,21 +11,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+// 后台类目列表修改操作控制层
 @WebServlet(name = "admin_type_edit",urlPatterns = "/admin/type_edit")
 public class AdminTypeEditServlet extends HttpServlet {
+    // 私有化业务层
     private TypeService tService = new TypeService();
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 创建Type对象
         Type t = new Type();
         try {
+            // 使用工具类BeanUtils，将请求中获取到的变量Map依次存入Type对象中
             BeanUtils.copyProperties(t, request.getParameterMap());
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        // 调用业务层，更新类型数据
         tService.update(t);
+        // 将请求转发到AdminTypeListServlet，更新后台管理页数据
         request.getRequestDispatcher("/admin/type_list").forward(request, response);
     }
 
