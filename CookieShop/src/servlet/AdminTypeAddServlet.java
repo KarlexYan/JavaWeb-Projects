ab20c30e3c4c385b47aba89e2c2232d960171670
KarlexYan <KarlexYan@aliyun.com>
2023-02-02 16:43:21 +0800

    后台添加类目控制层

--- CookieShop/src/servlet/AdminTypeAddServlet.java
+++ CookieShop/src/servlet/AdminTypeAddServlet.java
@@ -10,15 +10,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+// 后台添加类目控制层
 @WebServlet(name = "admin_type_add",urlPatterns = "/admin/type_add")
 public class AdminTypeAddServlet extends HttpServlet {
+    // 私有化业务层
     private TypeService tService = new TypeService();
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 从请求中获取到变量name的值，即要添加的类目名称
         String name = request.getParameter("name");
+        // 调用业务层，新增类目数据
         tService.insert(new Type(name));
+        // 将请求转发到AdminTypeListServlet，更新后台管理页数据
         request.getRequestDispatcher("/admin/type_list").forward(request, response);
     }
 
