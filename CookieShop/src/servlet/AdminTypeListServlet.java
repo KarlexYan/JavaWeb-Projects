135b7ae5fb1533ef3c74a1d0f8573ad8ceeaad2c
KarlexYan <KarlexYan@aliyun.com>
2023-02-02 16:43:50 +0800

    后台类目管理控制层

--- CookieShop/src/servlet/AdminTypeListServlet.java
+++ CookieShop/src/servlet/AdminTypeListServlet.java
@@ -10,18 +10,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
-
+// 后台类目管理控制层
 @WebServlet(name = "admi_type_list",urlPatterns = "/admin/type_list")
 public class AdminTypeListServlet extends HttpServlet {
+    // 私有化类型业务层对象
     private TypeService tService = new TypeService();
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 调用类型业务层，获得所有类型的表
         List<Type> list= tService.GetAllType();
+        // 在请求中写入list
         request.setAttribute("list", list);
+        // 更新Servlet中的变量typeList
 		this.getServletContext().removeAttribute("typeList");
         this.getServletContext().setAttribute("typeList",list);
+        // 将请求转发到后台类目管理列表页
         request.getRequestDispatcher("/admin/type_list.jsp").forward(request, response);
     }
 
